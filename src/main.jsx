eae93457f1abc127bc5a2d084caae24d7c854aef
ChumbaJ <step130202@yandex.ru>
2024-04-04 15:17:05 +0300

    feat: Add routing into app
    > index page /
    > registration /sign_up
    
    RegistrationDialog is renamed into Registration

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Registration from './routes/Registration.jsx';
+import { CssBaseline } from '@mui/material';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <></>
+  },
+  {
+    path: "/sign_up",
+    element: <Registration/>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <CssBaseline>
+      <RouterProvider router={router} />
+    </CssBaseline>
   </React.StrictMode>,
 )
